e42e1eceed74383d23058f933d2fad1ac116b4b4
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-06-08 00:37:16 +0300

    added cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,23 @@ var sass = require('gulp-sass');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
 gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass
-      .pipe(postcss([ autoprefixer() ]))
-      .pipe(gulp.dest('app/css'))
-  });
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+}); 
