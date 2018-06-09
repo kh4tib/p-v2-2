8112103c2750d1b1206b797d19c7fd788536cf9d
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-06-09 10:56:03 +0300

    added gulp-concat, gulp-rename and fixed the build all

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,43 @@
 var gulp = require('gulp');
 // SASS Tasks
 var sass = require('gulp-sass');
-// CSS Tasks
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var rename = require("gulp-rename");
 // JS Tasks
-var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+var pump = require('pump');
 
 // Build SASS
 gulp.task('build-sass', function(){
   return gulp.src('app/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(postcss([ autoprefixer() ]))
+    .pipe(rename({
+      basename: "style",
+      suffix: ".min",
+      extname: ".css"
+    }))
     .pipe(gulp.dest('dist/css'))
 });
 
 // Build JS
-gulp.task('build-js', function(){
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
-}); 
+gulp.task('build-js', function (cb) {
+  pump([
+        gulp.src('app/js/**/*.js'),
+        concat('all.js'),
+        uglify(),
+        rename({
+          basename: "main",
+          suffix: ".min",
+          extname: ".js"
+        }),
+        gulp.dest('dist/js')
+    ],
+    cb
+  );
+});
 
-//Build CSS
-// gulp.task('build-css', function(){
-//   return gulp.src('app/*.html')
-//     .pipe(useref())
-//     .pipe(gulpIf('*.css', cssnano()))
-//     .pipe(gulp.dest('dist'))
-// }); 
-
+// Build All
 gulp.task('build', ['build-sass', 'build-js'])
