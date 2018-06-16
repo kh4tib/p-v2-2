0b491914270bafc54fcdb28e6656e6643997fc12
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-06-16 13:50:23 +0300

    replaced random-images script to dist and added 404 page content

--- dist/js/random-images.js
+++ dist/js/random-images.js
@@ -4,6 +4,7 @@ var randomImageArray = [
     "https://media.giphy.com/media/3oGRFvUEbJdLWlawLu/giphy.gif",
     "https://media.giphy.com/media/NQL7Wuo2JSQSY/giphy.gif",
     "https://media.giphy.com/media/jO161HOYUPKEM/giphy.gif",
+    "https://media.giphy.com/media/hEc4k5pN17GZq/giphy.gif",
 ];
 
 function getRandomImage(imgAr) {
@@ -11,7 +12,7 @@ function getRandomImage(imgAr) {
     var img = imgAr[ num ];
     var imgStr = 'url('+ img + ')';
 
-    var sectionFourOFour = document.querySelector(".section__404");
+    var sectionFourOFour = document.querySelector(".section__not-found");
     sectionFourOFour.style.backgroundImage = imgStr;
 }
 
