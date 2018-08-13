32f6541f28ffec9d6bd1b709d0b56bb5e9e35e0b
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-08-13 16:32:13 +0300

    made some styling tweaks and added github link

--- app/js/contact-interaction.js
+++ app/js/contact-interaction.js
@@ -3,6 +3,7 @@ var emailLink = document.getElementsByClassName('js-email')[0];
 var twitterLink = document.getElementsByClassName('js-twitter')[0];
 var instagramLink = document.getElementsByClassName('js-instagram')[0];
 var dribbbleLink = document.getElementsByClassName('js-dribbble')[0];
+var githubLink = document.getElementsByClassName('js-github')[0];
 
 function contactHoverEffect(item, newContent, oldContent, theme) {
     item.addEventListener('mouseover', function(){
@@ -16,10 +17,12 @@ function contactHoverEffect(item, newContent, oldContent, theme) {
     });
 };
 
-contactHoverEffect(emailLink,'mo[at]khatib[dot]work','send me an email','--project-theme-email');
+contactHoverEffect(emailLink,'mo[at]khatib.work','send me an email','--project-theme-email');
 
 contactHoverEffect(twitterLink,'twitter.com/khatib', 'tweet me something', '--project-theme-twitter');
 
-contactHoverEffect(instagramLink,'instagram.com/kh4tib', 'moments i captured', '--project-theme-instagram');
+contactHoverEffect(instagramLink,'instagram.com/kh4tib', 'see my stories', '--project-theme-instagram');
 
 contactHoverEffect(dribbbleLink,'dribbble.com/khatib', 'check my dribbble','--project-theme-dribbble');
+
+contactHoverEffect(githubLink,'github.com/kh4tib', 'fork my code','--project-theme-github');
