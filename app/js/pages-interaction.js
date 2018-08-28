913bc1346d2cf4427beb9c5b1859ef5d2b8283e1
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-08-29 00:08:27 +0300

    added framesofmind page on homepage

--- app/js/pages-interaction.js
+++ app/js/pages-interaction.js
@@ -2,6 +2,7 @@ var eveTarget = document.getElementsByClassName('js-eve')[0];
 var flowTarget = document.getElementsByClassName('js-flow')[0];
 var santeTarget = document.getElementsByClassName('js-sante')[0];
 var exquisiteTarget = document.getElementsByClassName('js-exquisite')[0];
+var framesTarget = document.getElementsByClassName('js-frames')[0];
 var body = document.querySelector('body');
 
 function addTheme(target, theme) {
@@ -18,3 +19,4 @@ addTheme(eveTarget, '--project-theme-eve');
 addTheme(flowTarget, '--project-theme-flow');
 addTheme(santeTarget, '--project-theme-sante');
 addTheme(exquisiteTarget, '--project-theme-exquisite');
+addTheme(framesTarget, '--project-theme-frames');
