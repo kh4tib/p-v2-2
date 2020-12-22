eb308e144737687d8785b8060e657abc39a8d395
kh4tib <mhkhatibb@outlook.com>
2020-12-22 17:27:48 +0200

    update about page and contact

--- app/js/contact-interaction.js
+++ app/js/contact-interaction.js
@@ -3,7 +3,7 @@ let emailLink = document.getElementsByClassName('js-email')[0];
 let twitterLink = document.getElementsByClassName('js-twitter')[0];
 let instagramLink = document.getElementsByClassName('js-instagram')[0];
 let dribbbleLink = document.getElementsByClassName('js-dribbble')[0];
-let githubLink = document.getElementsByClassName('js-github')[0];
+let linkedinLink = document.getElementsByClassName('js-linkedin')[0];
 
 const contactHoverEffect = (item, newContent, oldContent, theme) => {
     item.addEventListener('mouseover', function(){
@@ -21,4 +21,4 @@ contactHoverEffect(emailLink,'email','📥','--project-theme-email');
 contactHoverEffect(twitterLink,'twitter', '🐣', '--project-theme-twitter');
 contactHoverEffect(instagramLink,'instagram', '🤳', '--project-theme-instagram');
 contactHoverEffect(dribbbleLink,'dribbble', '🏀','--project-theme-dribbble');
-contactHoverEffect(githubLink,'github', '👾','--project-theme-github');
+contactHoverEffect(linkedinLink,'linkedin', '👔','--project-theme-linkedin');
